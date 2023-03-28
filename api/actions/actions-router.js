fd1febaa7fdd19d1328cf8928d0479bd2f746496
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-28 19:30:08 +0300

    action router get istekleri olusturuldu

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,30 @@
 // "eylem" routerını buraya yazın
+const express = require('express');
+const router = express.Router();
+const actionModel = require('./actions-model');
+const middleware = require('./actions-middlware');
+
+router.get('/',async (req,res,next)=>{
+    try {
+        const allActions = await actionModel.get();
+        res.status(200).json(allActions);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:id',async (req,res,next)=>{
+    try {
+        const action = await actionModel.get(req.params.id);
+        if(action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json({message:'aksiyon bulunamadi'});
+        }
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = express();
 const projectRouter = require('./projects/projects-router');
+const actionRouter = require('./actions/actions-router');
 
 // Sunucunuzu yapılandırın
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
@@ -10,5 +11,6 @@ const projectRouter = require('./projects/projects-router');
 server.use(express.json());
 
 server.use('/api/projects',projectRouter);
+server.use('/api/actions',actionRouter);
 
 module.exports = server;
